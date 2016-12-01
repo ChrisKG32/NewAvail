a61e50ec23963b0cedbd75f843aedad15cce7c63
inten03 <inten03@gmail.com>
2016-12-01 14:46:26 -0600

    Update methods.js

--- app/server/methods.js
+++ app/server/methods.js
@@ -7,9 +7,3 @@ Meteor.methods({
     // server method logic
   }
 });
-
-S3.config = {
-    key: 'AKIAINIPDESKZVLPGU6Q',
-    secret: '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',
-    bucket: 'availbucket'
-};
